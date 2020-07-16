0cbd78079dca28a42d068a8242e64557bdf1b27e
Destiny Erhabor <destinterhabor6@gmail.com>
2020-07-16 13:02:30 +0100

    iterating through maps And intro to weakMaps

--- Working With Maps/js/page.js
+++ Working With Maps/js/page.js
@@ -13,50 +13,76 @@ var yearlyTotal = 0;
 
 const monthlySales = new Map();
 
+{
+    let salesA = {
+        a:[1,2]
+    }
+    var map = new WeakMap();
+    map.set(salesA, 'Hiking');
+
+    console.log('First ' + salesA);
+    
+}
+
+
 // Add Sales
 function addSale(){
-    monthlySales.set(newMonth.value, newAmount.value);
-    console.log(monthlySales);
+    monthlySales.set(newMonth.value, parseInt(newAmount.value));
+    
+    //update our label
+    monthlySales.data.labels = Array.from(monthlySales.keys());
+  
+    yearlyTotal = 0;
+    monthlySalesChart.data.datasets.forEach((dataset)=>{
+        dataset.data = [];
+    })
+
+    for (let amount of monthlySales.values()) {
+        yearlyTotal = amount + yearlyTotal;
+        yearlyLabel.innerHTML = yearlyTotal;
+
+        monthlySalesChart.data.datasets.forEach((dataset)=>{
+            dataset.data.push(amount);
+        })
+    }
+
+    monthlySales.update();
     
 }
 //find a value from map
-function findSale(){
-    // console.log(monthlySales.get(1));
-    // console.log(monthlySales.has(1));
-    
+function findSale(){    
 }
 //delete 
 function deleteVal() {
-    // monthlySales.delete(1);
-    // console.log(monthlySales);
+    
 }
 
 // Bar chart
-// var monthlySalesChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: [],
-//         datasets: [{
-//             label: '# of Sales',
-//             data: [],
-//             backgroundColor: [
-//                 'rgba(238, 184, 104, 1)',
-//                 'rgba(75, 166, 223, 1)',
-//                 'rgba(239, 118, 122, 1)',
-//             ],
-//             borderWidth: 0
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
+var monthlySalesChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '# of Sales',
+            data: [],
+            backgroundColor: [
+                'rgba(238, 184, 104, 1)',
+                'rgba(75, 166, 223, 1)',
+                'rgba(239, 118, 122, 1)',
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 
 // Pie Chart
 // var deptSalesChart = new Chart(pieCtx, {
